0a9b3ef041016b382103d70b0c76369eed151377
zhiriezra <zhiriezra@gmail.com>
2024-12-17 10:26:38 +0100

    view clearing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ asset('assets/images/favicon.png') }}" rel="icon">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
